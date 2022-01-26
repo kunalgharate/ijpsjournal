ff7944181807e11213f91aef1faa188922813477
Nellodia <nellodia@gmail.com>
2022-01-26 21:17:51 +0300

    Create header_responsive.php
    With a deleted duplicated stylesheet

---
+++ application/views/includes/header_responsive.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from www.ijprajournal.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 05 Sep 2021 07:01:26 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>ijpsjournal</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- site icons -->
+    <link rel="icon" href="<?php echo base_url();?>assets/images/fevicon/fevicon.jpg" type="image/gif" />
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
+    <!-- Site css -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css" />
+    <!-- responsive css -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/responsive.css" />
+    <!-- colors css -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/colors1.css" />
+    <!-- custom css -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/custom.css" />
+    <!-- wow Animation css -->
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/animate.css" />
+    <!-- revolution slider css -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/revolution/css/settings.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/revolution/css/layers.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/revolution/css/navigation.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css"
+          integrity="sha256-2XFplPlrFClt0bIdPgpz8H7ojnk10H69xRqd9+uTShA=" crossorigin="anonymous" />
+
+
+    <!--Animation-->
+
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+</head>
+<body id="default_theme" class="it_service">
+    <!-- loader -->
+    <!-- <div class="bg_load"> <img class="loader_animation" src="<?php echo base_url();?>assets/images/loaders/loader_1.png" alt="#" /> </div>
+    end loader -->
+    <!-- header -->
+<header id="default_header" class="header_style_1">
+    <!-- header top -->
+    <div class="header_top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="full">
+                        <div class="topbar-left">
+                            <ul class="list-inline">
+                                <!-- <li> <span class="topbar-label"><i class="fa  fa-home"></i></span> <span class="topbar-hightlight">540 Lorem Ipsum New York, AB 90218</span> </li> -->
+                                <li>
+                                    <span class="topbar-label" style="color:#ffffff">
+                                        <i style="padding:4px;" class="fa fa-envelope-o" ></i>
+                                    </span>
+                                    <span class="topbar-hightlight">
+                                        <a href="mailto:info@ijpsjournal.com">info@ijpsjournal.com</a>
+                                    </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 right_section_header_top">
+                    <div class="float-left">
+
+                    </div>
+                 <!--   <div class="float-right">
+                        <div class="make_appo">
+                            <a class="btn white_btn" href="<?php /*echo base_url();*/?>MenuScript">Submit Article</a>
+                        </div>
+                    </div>-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end header top -->
+    <!-- header bottom -->
+    <div class="header_bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+                    <!-- logo start -->
+                    <div class="logo">
+                        <a href="<?php echo base_url();?>">
+                            <img src="<?php echo base_url();?>assets/images/logos/logo.webp" alt="logo" />
+                        </a>
+                    </div>
+                    <!-- logo end -->
+                </div>
+                <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
+                    <!-- menu start -->
+                <div class="nav-uppar"><p>An ISO certified peer-reviewed journal</p>
+            
+                <div class="social_icon">
+
+                          <li>
+                                <span class="topbar-label">
+                                    <span class="topbar-hightlight">
+                                        <a style="color:#2E88C9;margin-left: 10px;" href="#" >
+                                            <b>ISSN: 0000-0000</b>
+                                        </a>
+                                    
+                                    
+                                    
+                                    </span>
+                            </li >
+                        
+                        <ul class="list-inline">
+                        
+              
+                             <li ><a class="fa fa-facebook" href="https://www.facebook.com/International-Journal-in-Pharmaceutical-Sciences-106918958461959" title="Facebook" target="_blank"></a></li>
+                               <!-- <li><a class="fa fa-google-plus" href="https://plus.google.com/" title="Google+" target="_blank"></a></li>-->
+                                <li><a class="fa fa-twitter" href="https://twitter.com/ijps_journal" title="Twitter" target="_blank"></a></li>
+                                <li><a class="fa fa-linkedin" href="https://linkedin.com/in/ijps-journal-036840221" title="LinkedIn" target="_blank"></a></li>
+                                <li><a class="fa fa-instagram" href="https://www.instagram.com/ijps_journal/" title="Instagram" target="_blank"></a></li>
+                        </ul>
+                    </div>
+                </div>
+                    <div class="menu_side">
+                        <div id="navbar_menu">
+                            <ul class="first-ul">
+                                <li>
+                                    <a class="active" href="<?php echo base_url();?>">Home</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url();?>#">About</a>
+                                    <ul>
+                                        <li>
+                                            <a href="<?php echo base_url();?>about">About Us</a>
+                                        </li>
+                                        <li>
+                                            <a href="<?php echo base_url();?>publicationarea">Publication Area</a>
+                                        </li>
+                                        <li>
+                                            <a href="<?php echo base_url();?>ReviwerGuidlines">Reviewer Guidelines</a>
+                                        </li>
+                                        <li>
+                                            <a href="<?php echo base_url();?>plagirism">Plagiarism Policy</a>
+                                        </li>
+                                        <li>
+                                            <a href="<?php echo base_url();?>accesspolicy">Open Access Policy</a>
+                                        </li>
+                                                                <li>
+                                            <a href="<?php echo base_url();?>editorialpolicy">Editorial Policy</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <!--  <li>
+                                    <a href="<?php echo base_url();?>ImpactFactor">Impact Factor</a>
+                                </li>-->
+                                <li>
+                                    <a href="#">For Authors</a>
+                                    <ul>
+                                        <li>
+                                            <a href="<?php echo base_url();?>CallForPaper">Call for Paper</a>
+                                        </li>
+                                       <!--- <li>
+                                            <a href="<?php echo base_url();?>Topics">Topics</a>
+                                        </li>--->
+                                        <li>
+                                            <a href="<?php echo base_url();?>AuthorGuidelines">Author Guidelines</a>
+                                        </li>
+                                        <li>
+                                            <a href="<?php echo base_url();?>CopyrightForm">Copyright Form</a>
+                                        </li>
+
+                                        <li>
+                                            <a href="<?php echo base_url();?>Modelscript">Model Manuscript</a>
+                                        </li>
+                                        <li>
+                                            <a href="<?php echo base_url();?>CheckPaperStatus">Check paper Status</a>
+                                        </li>
+                                                                <li>
+                                            <a href="<?php echo base_url();?>PeerReviewProcess">Peer Review Process</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url();?>Manuscript">Submit Manuscript</a>
+                                </li>
+                                <li>
+                                    <a href="#">Issue</a>
+                                    <ul>
+                                        <li ><a href="<?php echo base_url();?>CurrentIssue">Current Issue</a></li>                                       
+                                        <li>
+                                            <a href="<?php echo base_url();?>Archive">Archive</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url();?>EditorialBoard">Editorial Board</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url();?>PayFee">Pay Fees</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url();?>ContactUs">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!--  <div class="search_icon">
+                               <ul>
+                                 <li><a href="#" data-toggle="modal" data-target="#search_bar"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+                               </ul>
+                             </div> -->
+                    </div>
+                    <!-- menu end -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- header bottom end -->
+</header>
+ 
